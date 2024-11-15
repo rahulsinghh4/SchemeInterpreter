990661ce5782ba7e92c341d23487704bf69a1665
Rahul Singh <rahulsusa@gmail.com>
2024-11-15 01:47:45 -0500

    first commit

---
+++ questions.scm
@@ -0,0 +1,83 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+; Some utility functions that you may find useful to implement
+
+(define (zip pairs)
+  'replace-this-line)
+
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 15
+  (define (enum-helper s i)
+    (if (null? s) s
+    (cons (list i (car s)) (enum-helper (cdr s) (+ i 1)))))
+  (enum-helper s 0)
+  )
+  ; END PROBLEM 15
+
+;; Problem 16
+
+;; Merge two lists LIST1 and LIST2 according to COMP and return
+;; the merged lists.
+(define (merge comp list1 list2)
+  ; BEGIN PROBLEM 16
+    (cond 
+        ((null? list1) list2)
+        ((null? list2) list1)
+        (else 
+              (if (comp (car list1) (car list2))
+                  (cons (car list1) (merge comp (cdr list1) list2))
+                  (cons (car list2) (merge comp (cdr list2) list1))))))
+  ; END PROBLEM 16
+
+
+;; Problem 17
+
+;; Returns a function that checks if an expression is the special form FORM
+(define (check-special form)
+  (lambda (expr) (equal? form (car expr))))
+
+(define lambda? (check-special 'lambda))
+(define define? (check-special 'define))
+(define quoted? (check-special 'quote))
+(define let?    (check-special 'let))
+
+;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define (let-to-lambda expr)
+  (cond ((atom? expr)
+         ; BEGIN PROBLEM 17
+         'replace-this-line
+         ; END PROBLEM 17
+         )
+        ((quoted? expr)
+         ; BEGIN PROBLEM 17
+         'replace-this-line
+         ; END PROBLEM 17
+         )
+        ((or (lambda? expr)
+             (define? expr))
+         (let ((form   (car expr))
+               (params (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 17
+           'replace-this-line
+           ; END PROBLEM 17
+           ))
+        ((let? expr)
+         (let ((values (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 17
+           'replace-this-line
+           ; END PROBLEM 17
+           ))
+        (else
+         ; BEGIN PROBLEM 17
+         'replace-this-line
+         ; END PROBLEM 17
+         )))
+
